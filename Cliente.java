0f8403ed4a77847338a413c3a2fcefc8c4f40cae
legianesouza <111586772+legianesouza@users.noreply.github.com>
2026-03-05 17:17:56 -0300

    Add Cliente class with order and email service

---
+++ Cliente.java
@@ -0,0 +1,11 @@
+package main.java;
+
+//exemplo de dependencia.
+public class Cliente {
+
+    public void fazerPedido(ServicoEmail servico) {
+        System.out.println("Pedido realizado com sucesso.");
+        servico.enviarEmail("Seu pedido foi confirmado e já está em preparo!");
+    }
+
+}
