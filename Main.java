78c45ee5b7a0ed3180cd809b2eed3154ba97d893
legianesouza <111586772+legianesouza@users.noreply.github.com>
2026-03-05 17:29:18 -0300

    Add Escola and Cliente classes with example usage

--- Main.java
+++ Main.java
@@ -17,6 +17,22 @@ public class Main {
 		Trator trator1 = new Trator("Azul", "New Holland 7630", 2020, "Motor Genesis 110cv", "Tração 4x4", "Agricultura");
 		trator1.ligar_carro();
 		trator1.buzinar();
+
+
+		Professor professor1 = new Professor("José", 8978.00);
+		Professor professor2 = new Professor("Daniel", 9900.00);
+		Professor professor3 = new Professor("Paulo", 5220.00);
+		
+		Professor[] professores1 = {professor1, professor2, professor3};
+		
+		Escola livraria1 = new Escola("Escola Estadual Dr Caetano Munhoz Rocha", 1927, professores1);
+		livraria1.mostraInfo();
+		
+		
+		Cliente cliente = new Cliente();
+        ServicoEmail servico = new ServicoEmail();
+
+        cliente.fazerPedido(servico);
 	
 	}
 	
